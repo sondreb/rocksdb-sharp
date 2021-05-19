6c102bb4d31edcd8dcedce7c5ff7854f3dcb59df
Sondre <sondre@outlook.com>
2021-05-19 20:17:28 +0200

    Update build-rocksdb.sh

--- build-native/build-rocksdb.sh
+++ build-native/build-rocksdb.sh
@@ -144,6 +144,7 @@ else
         RUNTIME=linux-x64
         # Linux Dependencies    
         sudo apt-get install libsnappy-dev libbz2-dev libz-dev liblz4-dev libzstd-dev
+        sudo apt-get install libgflags-dev, zlib1g-dev
     fi
     
     mkdir -p rocksdb || fail "unable to create rocksdb directory"
